28137aed1242c5e0441cfb11a9bd1aab6e1f8677
Felipe <zfelipesilveira@gmail.com>
2022-06-13 15:53:53 -0300

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('title', 'slug', 'author', 'publish', 'status')
+    search_fields = ('title', 'body') 
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
